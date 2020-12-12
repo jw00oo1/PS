ca22ea32fae369b07561b62e8ae7cdde7193fc7f
jw00oo1 <53477630+jw00oo1@users.noreply.github.com>
2020-12-13 01:34:33 +0900

    Create 기능개발.cpp
    stack/queue

---
+++ Programmers/lv2/기능개발.cpp
@@ -0,0 +1,41 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+    vector<int> answer;
+    vector<int> days;
+
+    for(int i = 0; i < progresses.size(); i++)
+    {
+        int rest = 100 - progresses[i];
+        int day = 0, speed = speeds[i];
+        if(rest % speed)
+        {
+            day = rest / speed + 1;
+        }
+        else day = rest / speed;
+
+        days.push_back(day);
+    }
+
+    int mxm = days[0], cnt = 0;
+    for(int i = 0; i < days.size(); i++)
+    {
+        if(mxm >= days[i])
+        {
+            cnt++;
+        }
+        else
+        {
+            mxm = days[i];
+            answer.push_back(cnt);
+            cnt = 1;
+        }
+    }
+    answer.push_back(cnt);
+
+    return answer;
+}
