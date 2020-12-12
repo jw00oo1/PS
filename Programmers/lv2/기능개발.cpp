39aa8d859a67d701bd0edbd78e066d091756ede1
jw00oo1 <53477630+jw00oo1@users.noreply.github.com>
2020-12-13 01:38:21 +0900

    Update 기능개발.cpp

--- Programmers/lv2/기능개발.cpp
+++ Programmers/lv2/기능개발.cpp
@@ -7,9 +7,13 @@ using namespace std;
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
     vector<int> days;
-
+    
     for(int i = 0; i < progresses.size(); i++)
     {
+          //this can be changed below:
+          // day = (99 - progresses[i]) / speeds[i] + 1;
+          // days.push_back(day);
+        
         int rest = 100 - progresses[i];
         int day = 0, speed = speeds[i];
         if(rest % speed)
